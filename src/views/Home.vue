eee1d5256bc33f0816172380c1b899208ebf4b8f
David Zimmermann <s0537835@htw-berlin.de>
2021-12-21 14:25:10 +0100

    added .env files

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -13,6 +13,24 @@ export default {
   name: 'Home',
   components: {
     HelloWorld
+  },
+  data () {
+    return {
+      users: []
+    }
+  },
+  mounted () {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    }
+
+    fetch('http://localhost:3000/api/v1/home', requestOptions)
+      .then(response => response.json())
+      .then(result => result.foreach(user => {
+        this.users.push(user)
+      }))
+      .catch(error => console.log('errer', error))
   }
 }
 </script>
