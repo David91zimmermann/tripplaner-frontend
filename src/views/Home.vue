0f00d4f68c4020fa338e13e22cd127426472d360
David Zimmermann <s0537835@htw-berlin.de>
2021-12-21 17:23:12 +0100

    added User view and editting

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,29 +8,10 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'Home',
   components: {
     HelloWorld
-  },
-  data () {
-    return {
-      users: []
-    }
-  },
-  mounted () {
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow'
-    }
-
-    fetch('http://localhost:3000/api/v1/home', requestOptions)
-      .then(response => response.json())
-      .then(result => result.foreach(user => {
-        this.users.push(user)
-      }))
-      .catch(error => console.log('errer', error))
   }
 }
 </script>
---
+++ src/views/User.vue
@@ -0,0 +1,54 @@
+<template>
+  <h1>Welcome to User</h1>
+  <div class="container-fluid">
+    <users-card-list :users="this.users"></users-card-list>
+  </div>
+  <users-create-form @created="addUser"></users-create-form>
+</template>
+
+<script>
+import UsersCardList from '@/components/UsersCardList'
+import UsersCreateForm from '@/components/UsersCreateForm'
+
+export default {
+  name: 'Users',
+  components: {
+    UsersCardList,
+    UsersCreateForm
+  },
+  data () {
+    return {
+      users: []
+    }
+  },
+  methods: {
+    addUser (userLocation) {
+      const endpoint = process.env.VUE_APP_BACKEND_BASE_URL + userLocation
+      const requestOptions = {
+        method: 'GET',
+        redirect: 'follow'
+      }
+      fetch(endpoint, requestOptions)
+        .then(response => response.json())
+        .then(user => this.users.push(user))
+        .catch(error => console.log('error', error))
+    }
+  },
+  mounted () {
+    const endpoint = process.env.VUE_APP_BACKEND_BASE_URL + '/api/v1/users'
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    }
+    fetch(endpoint, requestOptions)
+      .then(response => response.json())
+      .then(result => result.forEach(user => {
+        this.users.push(user)
+      }))
+      .catch(error => console.log('error', error))
+  }
+}
+</script>
+
+<style scoped>
+</style>
