894af9ea3c707437406473d005b0295586e12d82
David Zimmermann <s0537835@htw-berlin.de>
2022-01-07 11:32:44 +0100

    added Sidebar

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>The Sidebar is {{ collapsed ? 'closed' : 'open' }}</h1>
+  <button @click="toggleSidebar">Toggle Sidebar</button>
+</template>
+
+<script>
+import { collapsed, toggleSidebar } from '@/components/sidebar/state'
+
+export default {
+  setup () {
+    return { collapsed, toggleSidebar }
+  },
+  name: 'Dashboard'
+}
+</script>
+
+<style scoped>
+
+</style>
